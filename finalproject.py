88f4b04d8b8e8d73b491ff6055d74a05f510a5c5
Brokoli <bocalist@gmail.com>
2018-10-12 17:04:22 +0900

    Added some routing

--- finalproject.py
+++ finalproject.py
@@ -1,8 +1,49 @@
-from flalsk import flask
+from flask import Flask
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants')
+def showRestaurants():
+    return "This is Index Page."
+
+@app.route('/restaurant/new')
+def newRestaurant():
+    return "Add new restaurant."
+
+@app.route('/restaurant/<int:restaurant_id>/edit')
+def editRestaurant(restaurant_id):
+    return "Edit restaurant information."
+
+@app.route('/restaurant/<int:restaurant_id>/delete')
+def deleteRestaurant(restaurant_id):
+    return "Delete restaurant information."
+
+@app.route('/restaurant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def showMenu(restaurant_id):
+    return "This is Menu Page."
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def newMenuItem(restaurant_id):
+    return "Add new menu item"
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "Edit menu item"
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "Delete menu item"
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
-    
